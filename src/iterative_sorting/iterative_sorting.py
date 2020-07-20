2f2ef6f97dd7141c6cd51566926ab2a81a35669d
Robert Buckley <robertworkbuckley@gmail.com>
2020-07-20 15:12:15 -0700

    module complete

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -4,13 +4,11 @@ def selection_sort(arr):
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
-        # Your code here
 
-
-        # TO-DO: swap
-        # Your code here
+        for x in range(smallest_index, len(arr)):
+            if arr[smallest_index] > arr[x]:
+                smallest_index = x
+        arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
 
     return arr
 
@@ -18,11 +16,18 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-
+    swap = 1
+    while swap > 0:
+        swap = 0
+        for i in range(0, len(arr) - 1):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                swap += 1
 
     return arr
 
-'''
+
+"""
 STRETCH: implement the Counting Sort function below
 
 Counting sort is a sorting algorithm that works on a set of data where
@@ -38,9 +43,27 @@ showed up, we can construct a sorted set of the input data from the
 buckets. 
 
 What is the time and space complexity of the counting sort algorithm?
-'''
-def counting_sort(arr, maximum=None):
-    # Your code here
+"""
 
+
+def counting_sort(arr, maximum):
+    # Your code here
+    # new = []
+    if arr is []:
+        return arr
+    counts = [0] * (maximum + 1)
+    for i in range(len(arr)): # O(n)
+        value = arr[i]
+        if value < 0:
+            return "Error, negative numbers not allowed in Count Sort"
+        counts[value] = counts[value] + 1
+    curr_index = 0
+    for i in range(len(counts)): # O(n)
+        if counts[i] > 0:
+            value = counts[i]
+            for x in range(value): # O(n)
+                arr[curr_index] = i
+                curr_index += 1
+            #     new.append(i)
 
     return arr
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,63 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return True
 
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    first = 0
+    last = (len(arr) -1)
+
+    found = False
+
+    while first <= last and not found:
+
+        middle = (first + last) // 2
+
+        if arr[middle] == target:
+            found = True
+
+        else:
+            if target < arr[middle]:
+                 last = middle -1
+
+            else: 
+                first = middle + 1
+
+    return found
 
     # Your code here
 
 
     return -1  # not found
+
+
+def insertion_sort(input_list):
+    # separate the first element
+    # think of it as sorted
+    # no code required here / abstract step
+
+    # for all unsorted items
+    for i in range(1, len(input_list)):
+        # makr the current item we are considering
+        current = input_list[i]
+        
+        # look left until we find the proper place to insert current item
+        j = i 
+        while j > 0 and current < input_list[j - 1]:
+
+            # as we are "looking left", we need to shift items to the right
+            input_list[j] = input_list[j-1]
+            j -= 1
+
+        # when we've found our insertion point (j)
+            # insert item
+        input_list[j] = current
+
+    return input_list 
+
+print(insertion_sort([3,4,2,4,5,6,23]))
